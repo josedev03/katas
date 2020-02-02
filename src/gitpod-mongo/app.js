5049f346f6abe10b720d5fdb01540841b15703fe
josedev03 <joseing03@gmail.com>
2020-02-02 01:00:34 +0000

    se agrega la opcion de eliminar tareas

--- src/gitpod-mongo/app.js
+++ src/gitpod-mongo/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const Task = require('./model/task.model')
 
-// setings
+// settings
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 
@@ -23,6 +23,22 @@ app.post('/create', async (req, res) => {
   newTask.save()
   console.log(req.body)
   res.redirect('/')
+})
+
+app.delete('/delete', async (req, res)=>{
+  try {
+    await Task.deleteOne({_id: req.body.taskid})
+    res.status(200).json({
+      data: 'usuario eliminado',
+      erros: null
+    })
+  } catch (error) {
+    console.log(`Error al eliminar: ${error}`)
+    res.status(500).json({
+      data: '',
+      errors: error
+    })
+  }
 })
 
 app.use(express.static(__dirname + '/public'))
--- src/gitpod-mongo/database.js
+++ src/gitpod-mongo/database.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
-// mongodb+srv://jose:mongo@cluster0-vscks.mongodb.net/test?retryWrites=true&w=majority
-// mongodb+srv://admindev:<password>@mongojose-qohde.mongodb.net/test?retryWrites=true&w=majority
 mongoose.connect('mongodb+srv://admindev:jose1234@mongojose-qohde.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
